9f8309a813fdd9f5642d68b2c641c37db651eff2
Konynico <56192487+Konynico@users.noreply.github.com>
2023-01-24 11:48:03 +0100

    Add files via upload

--- R2D2.js
+++ R2D2.js
@@ -1,5 +1,7 @@
 console.log("test");
 
+var port = process.env['PORT'] || 3000;
+
 const Discord = require("discord.js");
 const loadCommands = require("./Loader/loadCommands");
 const loadEvents = require("./Loader/loadEvents");
