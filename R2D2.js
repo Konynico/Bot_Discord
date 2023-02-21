e44506100e273921ead07674d1dc4e1cc7b2c766
Konynico <56192487+Konynico@users.noreply.github.com>
2023-02-21 21:26:37 +0100

    Add files via upload

--- R2D2.js
+++ R2D2.js
@@ -1,12 +1,26 @@
 console.log("test");
-
-
-const Discord = require(".js");
+const Player = require("discord-player");
+const player = require('play-sound')
+const Discord = require("discord.js");
 const loadCommands = require("./Loader/loadCommands");
 const loadEvents = require("./Loader/loadEvents");
+const loadPlayerEvents = require("./Loader/loadPlayerEvent");
 const config = require("./config")
 
 bot = new Discord.Client({intents : 3276799})
+
+
+bot.player = new Player.Player(bot,{
+    leaveOnEnd: true,
+    leaveOnEmpty: true,
+    volume: 70,
+    ytdlOptions: {
+        filter: 'audioonly',
+        quality: 'highestaudio',
+        highWaterMark: 1 << 25,
+    },
+})
+
 
 const http = require('http');
 
@@ -21,10 +35,11 @@ bot.commands = new Discord.Collection()
 bot.color = "#0002E1";
 
 //bot.login(process.env.token);
+bot.login(config.token);
 
 loadCommands(bot)
 loadEvents(bot)
-
+loadPlayerEvents(bot)
 
 
 
