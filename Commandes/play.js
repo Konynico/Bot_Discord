681195ffa66bf630d278c58dde37dfbe9726e8e5
Konynico <56192487+Konynico@users.noreply.github.com>
2023-02-21 23:22:49 +0100

    Add files via upload

--- Commandes/play.js
+++ Commandes/play.js
@@ -25,7 +25,7 @@ module.exports = {
             if(!message.member.voice.channel) return message.reply("Vous devez être dans un salon vocal pour utiliser cette commande !")
             if((await message.guild.members.fetchMe()).voice.channel && (await message.guild.members.fetchMe()).voice.channel.id !== message.member.voice.channel.id) return message.reply("Vous devez être dans le même salon vocal que moi pour utiliser cette commande !")
 
-            message.deferReply()
+            await message.deferReply()
 
             const queue = await bot.player.createQueue(message.guild, {metadata : {message: message}})
 
@@ -34,5 +34,5 @@ module.exports = {
 
             if(!queue.connection) await queue.connect(message.member.voice.channel)
             await queue.play(track)
-            message.follwUp(`La musique **${track.title}** a été ajouté à la file d'attente !`)
+            message.editReply(`La musique **${track.title}** a été ajouté à la file d'attente !`)
 }}       
