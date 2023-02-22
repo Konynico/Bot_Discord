7a2462c76cca79ef0393f54bc710bfc3803a3c02
Konynico <56192487+Konynico@users.noreply.github.com>
2023-02-22 09:06:12 +0100

    Update stop.js

--- Commandes/stop.js
+++ Commandes/stop.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 
 module.exports = {
 
-        name: "stop",
+        name: "stop_M",
         description: "stop la musique",
         permissions: "Aucune",
         dm: false,
@@ -16,7 +16,7 @@ module.exports = {
 
           
             const queue = await bot.player.createQueue(message.guild, {metadata : {message: message}})
-            if(!queue.connection || !queu.playing) return message.reply("le bot n'est pas connecté ou ne joue pas de musique !");
+            if(!queue.connection || !queue.playing) return message.reply("le bot n'est pas connecté ou ne joue pas de musique !");
 
             queue.destroy();
             message.reply("La musique a été stoppé !")
