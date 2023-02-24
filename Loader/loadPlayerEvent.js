74364f19c60ddaf524bf0f7aa2e60dc3f2ae3503
Konynico <56192487+Konynico@users.noreply.github.com>
2023-02-24 11:17:22 +0100

    Add files via upload

--- Loader/loadPlayerEvent.js
+++ Loader/loadPlayerEvent.js
@@ -5,7 +5,7 @@ module.exports = async bot => {
     fs.readdirSync("./Events").filter(f => f.endsWith(".js")).forEach(async file => {
 
         let event = require(`../Events/${file}`)
-        bot.player .on(file.split(".js").join(""), event.bind(null, bot)) //parametres 
+        bot.player.on(file.split(".js").join(""), event.bind(null, bot)) //parametres 
 
     })
 }
