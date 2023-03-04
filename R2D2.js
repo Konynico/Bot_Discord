82bf5f5e7f09bd6d28bb76520f408f4cba4a7ce7
Konynico <56192487+Konynico@users.noreply.github.com>
2023-03-04 18:09:10 +0100

    Add files via upload

--- R2D2.js
+++ R2D2.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 const loadCommands = require("./Loader/loadCommands");
 const loadEvents = require("./Loader/loadEvents");
 const loadPlayerEvents = require("./Loader/loadPlayerEvent");
-
+const config = require("./config")
 
 bot = new Discord.Client({intents : 3276799})
 
@@ -34,7 +34,7 @@ bot.commands = new Discord.Collection()
 bot.color = "#0002E1";
 
 //bot.login(process.env.token);
-
+bot.login(config.token);
 
 loadCommands(bot)
 loadEvents(bot)
