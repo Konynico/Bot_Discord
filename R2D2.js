68bb897d3b4b809898e454dd4f5afb1dad22ef7b
Konynico <56192487+Konynico@users.noreply.github.com>
2023-03-04 18:14:35 +0100

    Add files via upload

--- R2D2.js
+++ R2D2.js
@@ -12,12 +12,41 @@ bot = new Discord.Client({intents : 3276799})
 bot.player = new Player.Player(bot,{
     leaveOnEnd: true,
     leaveOnEmpty: true,
-    volume: 10,
+    volume: 50,
+    leaveOnStop: true,
+    autoSelfDeaf: true,
     ytdlOptions: {
         filter: 'audioonly',
         quality: 'highestaudio',
         highWaterMark: 1 << 25,
+        opusEncoded: true,
+        encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+
     },
+    leaveOnEmptyCooldown: 0,
+    quality: 'high',
+    enableLive: true,
+    enableQueueStat: true,
+    retryLimit: 5,
+    timeout: 1800000,
+    nsfw: false,
+    emitNewSongOnly: false,
+    useSafeSearch: true,
+    initialVolume: 50,
+    sameVoiceChannel: true,
+    updateYouTubeDL: true,
+    youtubeDL: true,
+    youtubeCookie: null,
+    youtubeIdentityToken: null,
+    customFilters: null,
+    enableSoundcloud: true,
+    soundcloudClientID: null,
+    soundcloudCookie: null,
+    enableSpotify: true,
+    spotifyCookie: null,
+    spotifyClientID: null,
+    spotifyClientSecret: null,
+    spotifyAutoResolve: true,
 })
 
 
