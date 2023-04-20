8e8ab5dc3fce7be6a0972071eeba82cdb1ce5992
Konynico <56192487+Konynico@users.noreply.github.com>
2023-04-20 09:48:42 +0200

    Update R2D2.js

--- R2D2.js
+++ R2D2.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 const loadCommands = require("./Loader/loadCommands");
 const loadEvents = require("./Loader/loadEvents");
 const loadPlayerEvents = require("./Loader/loadPlayerEvent");
-const config = require("./config")
+
 
 bot = new Discord.Client({intents : 3276799})
 
